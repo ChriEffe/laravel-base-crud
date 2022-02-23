b86204184242aa55035ade3d0ffc79fdafb3e8ae
ChriEffe <christianferrazzano93@gmail.com>
2022-02-23 15:32:03 +0100

    add create.blade

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,9 +1,41 @@
 @extends('layout.base')
 
 @section('documentTitle')
-    {{-- {{ $title }} --}}
+    {{ $title }}
 @endsection
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <form action="{{ route('comics.store') }}" method="post">
+                @csrf
+                @method('POST')
+                <div class="mb-3">
+                    <label for="title" class="form-label">Title Comics</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="mb-3">
+                    <label for="author" class="form-label">Author</label>
+                    <input type="text" class="form-control" id="author" name="author">
+                </div>
+                <div class="mb-3">
+                    <label for="genre" class="form-label">Genre</label>
+                    <input class="form-control" id="genre" name="genre" rows="3">
+                </div>
+                <div class="mb-3">
+                    <label for="price" class="form-label">Price</label>
+                    <input type="text" class="form-control" id="price" name="price">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <textarea type="text" class="form-control" id="description" name="description"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="photo" class="form-label">Photo</label>
+                    <input type="text" class="form-control" id="photo" name="photo">
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
 @endsection
